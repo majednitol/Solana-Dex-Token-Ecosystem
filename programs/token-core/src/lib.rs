a18cd3c82840e0a55b79d9919181c8cfab007ffb
majednitol <majedrahman7@gmail.com>
2026-02-10 15:51:20 +0600

    fix inital error in anchor.toml

---
+++ programs/token-core/src/lib.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+
+// Program ID
+declare_id!("EKxbxCKrMseRkptPv1EVUgTv1uBW4WuSbvtrm6miwYLk");
+
+#[program]
+pub mod token_core_contracts {
+    use super::*;
+
+    // Hello world function
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        msg!("Hello, world!");
+        Ok(())
+    }
+}
+
+// Context for the initialize function
+#[derive(Accounts)]
+pub struct Initialize {}
